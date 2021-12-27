78516878eb9e1c9ed5a40b8af05e5cec932fcc7c
Ehsan Razmkhah <ehsan9891@gmail.com>
2021-12-27 22:03:26 +0100

    Create base member class and main menu

--- lib_nekoye/src/lib_nekoye/Main.java
+++ lib_nekoye/src/lib_nekoye/Main.java
@@ -1,9 +1,41 @@
 package lib_nekoye;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+
 public class Main {
 
+	private Array members;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello to my application!");
+		System.out.println("Please select an item from menu:");
+		System.out.println("1 - Create Members");
+		System.out.println("2 - List Members");
+		
+		try {
+			char menu_item;
+			
+			menu_item = (char)System.in.read();
+		
+		switch (menu_item) {
+		case '1': {
+
+			System.out.println("Welcome to Create member menu");
+			break;
+		}
+		case '2':
+			System.out.println("Welcome to List member menu");
+			break;
+			
+		default:
+			System.out.println("Unexpected value: " + menu_item);
+		}
+		}catch(IOException exception) {
+			System.out.println("Error reading from user");
+			
+		}
 
 	}
 
---
+++ lib_nekoye/src/lib_nekoye/Member.java
@@ -0,0 +1,46 @@
+package lib_nekoye;
+
+public class Member {
+	
+	public int id;
+	public String name;
+	public int age;
+	public String gender;
+	
+	public Member(Integer id, String name, Integer age,String gender) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return this.gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
